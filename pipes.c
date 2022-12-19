bd62c9df412defdb4dc7caf2522e017b1ac1b0fa
Nuno Videira <nvideira@c1r20s1.42lisboa.com>
2022-12-19 02:40:23 +0000

    started pipes

---
+++ pipes.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 02:22:13 by nvideira          #+#    #+#             */
+/*   Updated: 2022/12/19 02:40:02 by nvideira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	use_pipe(int *pip)
+{
+	if (pipe(pip) < 0)
+	{
+		write(2, "Error creating pipe\n", 20);
+		return (1);
+	}
+	com_info()->pid = fork();
+	if (com_info()->pid < 0)
+	{
+		write(2, "Error creating fork\n", 20);
+		return (1);
+	}
+	else if (com_info()->pid == 0)
+	{
+		dup2(pip[1], STDOUT_FILENO);
+		close(pip[0]);
+		close(pip[1]);
+	}
+	else
+	{
+		dup2(pip[0], STDIN_FILENO);
+		close(pip[0]);
+		close(pip[1]);
+	}
+}
